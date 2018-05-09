ab580beda1f8306c2f3808d7add57e3752d99961
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-05-09 18:08:04 +0700

    Restructure

--- Com.DanLiris.Service.Purchasing.WebApi/Startup.cs
+++ Com.DanLiris.Service.Purchasing.WebApi/Startup.cs
@@ -1,23 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Com.DanLiris.Service.Purchasing.Lib;
+using Com.DanLiris.Service.Purchasing.Lib.Facades.Expedition;
+using Com.DanLiris.Service.Purchasing.Lib.Services;
+using Com.DanLiris.Service.Purchasing.Lib.Services.Expedition;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Serialization;
+using System.Text;
 
-namespace Com.DanLiris.Service.Purchasing
+namespace Com.DanLiris.Service.Purchasing.WebApi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        /* Hard Code */
+        private string[] EXPOSED_HEADERS = new string[] { "Content-Disposition", "api-version", "content-length", "content-md5", "content-type", "date", "request-id", "response-time" };
+        private string PURCHASING_POLICITY = "PurchasingPolicy";
+        private string DEFAULT_CONNECTION = "DefaultConnection";
+        private string SECRET = "Secret";
+        
+        public IConfiguration Configuration { get; }
+
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        #region Register
+
+        private void RegisterEndpoints()
+        {
+
+        }
+
+        private void RegisterFacades(IServiceCollection services)
+        {
+            services
+                .AddTransient<PurchasingDocumentExpeditionFacade>();
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services
+                .AddTransient<PurchasingDocumentExpeditionService>()
+                .AddScoped<IdentityService>()
+                .AddScoped<HttpClientService>()
+                .AddScoped<ValidateService>();
+        }
+
+        #endregion Register
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = Configuration.GetConnectionString(DEFAULT_CONNECTION) ?? Configuration[DEFAULT_CONNECTION];
+
+            /* Register */
+            services.AddDbContext<PurchasingDbContext>(options => options.UseSqlServer(connectionString));
+            RegisterEndpoints();
+            RegisterFacades(services);
+            RegisterServices(services);
+
+            /* Versioning */
+            services.AddApiVersioning(options => { options.DefaultApiVersion = new ApiVersion(1, 0); });
+
+            /* Authentication */
+            string Secret = Configuration.GetValue<string>(SECRET) ?? Configuration[SECRET];
+            SymmetricSecurityKey Key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Secret));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateLifetime = false,
+                        IssuerSigningKey = Key
+                    };
+                });
+
+            /* CORS */
+            services.AddCors(options => options.AddPolicy(PURCHASING_POLICITY, builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .WithExposedHeaders(EXPOSED_HEADERS);
+            }));
+
+            /* API */
+            services
+               .AddMvcCore()
+               .AddAuthorization()
+               .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver())
+               .AddJsonFormatters();
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,10 +108,16 @@ namespace Com.DanLiris.Service.Purchasing
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            /* Update Database */
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                PurchasingDbContext context = serviceScope.ServiceProvider.GetService<PurchasingDbContext>();
+                context.Database.Migrate();
+            }
+
+            app.UseAuthentication();
+            app.UseCors(PURCHASING_POLICITY);
+            app.UseMvc();
         }
     }
 }
