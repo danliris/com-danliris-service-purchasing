3af48798b7959322528f58b728a493baea9e0575
kuswandanu <kuswandanu@gmail.com>
2018-06-12 12:15:46 +0700

    DeliveryOrder Model and Migration

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20180612022418_Delivery_Order.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20180612022418_Delivery_Order.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180612022418_Delivery_Order")]
+    partial class Delivery_Order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
