7adb4307fc7fd8f120ea6ed25dc0c6d112711249
ardana96 <ardanagama@gmail.com>
2020-04-24 16:12:48 +0700

    Update ReportTest.cs

--- Com.DanLiris.Service.Purchasing.Test/Facades/UnitPaymentOrderPaidStatusTests/ReportTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Facades/UnitPaymentOrderPaidStatusTests/ReportTest.cs
@@ -247,7 +247,7 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.UnitPaymentOrderPaidStatu
             var dateFrom = modelLocalSupplier.Date;
             var dateToDue = modelLocalSupplier.DueDate;
             var dateFromDue = modelLocalSupplier.DueDate;
-            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "LUNAS","","", dateFromDue, dateToDue, dateFrom, dateTo, 1);
+            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "", "", "LUNAS",dateFromDue, dateToDue, dateFrom, dateTo, 1);
             // var results = await facade.GetReport(25,1,"{}",modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, null, dateFromDue, dateToDue, dateFrom, dateTo, 1);
 
             Assert.NotNull(results.Data);
@@ -272,7 +272,7 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.UnitPaymentOrderPaidStatu
             var dateFrom = modelLocalSupplier.Date;
             var dateToDue = modelLocalSupplier.DueDate;
             var dateFromDue = modelLocalSupplier.DueDate;
-            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "SUDAH BAYAR DPP+PPN", "","",dateFromDue, dateToDue, dateFrom, dateTo, 1);
+            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "", "", "SUDAH BAYAR DPP+PPN", dateFromDue, dateToDue, dateFrom, dateTo, 1);
             // var results = await facade.GetReport(25,1,"{}",modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, null, dateFromDue, dateToDue, dateFrom, dateTo, 1);
 
             Assert.NotNull(results.Data);
@@ -297,7 +297,7 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.UnitPaymentOrderPaidStatu
             var dateFrom = modelLocalSupplier.Date;
             var dateToDue = modelLocalSupplier.DueDate;
             var dateFromDue = modelLocalSupplier.DueDate;
-            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "SUDAH BAYAR PPH","","", dateFromDue, dateToDue, dateFrom, dateTo, 1);
+            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "", "", "SUDAH BAYAR PPH", dateFromDue, dateToDue, dateFrom, dateTo, 1);
             // var results = await facade.GetReport(25,1,"{}",modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, null, dateFromDue, dateToDue, dateFrom, dateTo, 1);
 
             Assert.NotNull(results.Data);
@@ -322,7 +322,7 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.UnitPaymentOrderPaidStatu
             var dateFrom = modelLocalSupplier.Date;
             var dateToDue = modelLocalSupplier.DueDate;
             var dateFromDue = modelLocalSupplier.DueDate;
-            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "BELUM BAYAR", "","",dateFromDue, dateToDue, dateFrom, dateTo, 1);
+            var results = facade.GetReport(25, 1, "{}", modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, "", "", "BELUM BAYAR", dateFromDue, dateToDue, dateFrom, dateTo, 1);
             // var results = await facade.GetReport(25,1,"{}",modelLocalSupplier.UPONo, modelLocalSupplier.SupplierCode, modelLocalSupplier.DivisionCode, null, dateFromDue, dateToDue, dateFrom, dateTo, 1);
 
             Assert.NotNull(results.Data);
