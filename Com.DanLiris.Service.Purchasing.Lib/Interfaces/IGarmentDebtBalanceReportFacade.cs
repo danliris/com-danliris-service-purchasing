1502f98a1cbc5122b86cdde3ec34b9608b5f3d48
ANDRI PURNOMO <andrea.kinan@gmail.com>
2020-05-18 08:58:31 +0700

    Add Service Garment Debt Balance Report
    Add Service Garment Debt Balance Report

---
+++ Com.DanLiris.Service.Purchasing.Lib/Interfaces/IGarmentDebtBalanceReportFacade.cs
@@ -0,0 +1,14 @@
+﻿using Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentReports;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Com.DanLiris.Service.Purchasing.Lib.Interfaces
+{
+    public interface IGarmentDebtBalanceReportFacade
+    {
+        List<GarmentDebtBalanceFixViewModel> GetReport(bool jnsSpl, string payMtd, string category, DateTime? dateFrom, DateTime? dateTo, string Order, int offset);
+        MemoryStream GetXLs(bool jnsSpl, string payMtd, string category, DateTime? dateFrom, DateTime? dateTo, int offset);
+    }
+}
---
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentReports/GarmentDebtBalanceReportFixViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentReports
+{
+    public class GarmentDebtBalanceFixViewModel
+    {
+        public int count { get; set; }
+        public DateTimeOffset? ArrivalDate { get; set; }
+        public string SupplierCode { get; set; }
+        public string SupplierName { get; set; }
+        public string CurrencyCode { get; set; }
+        public string DONo { get; set; }
+        public string BillNo { get; set; }
+        public string CodeRequirement { get; set; }
+        public decimal DebtAmount { get; set; }
+        public string PaymentNo { get; set; }
+        public DateTimeOffset? PaymentDate { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public decimal DebtBalance { get; set; }
+    }
+}
