c40c38193a57048dca43ef869d0bb3047faa72a2
putrapradesa <49633721+putrapradesa@users.noreply.github.com>
2020-09-03 13:51:04 +0700

    Add BalanceStocks

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20200903064043_Add_BalanceStock.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20200903064043_Add_BalanceStock.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200903064043_Add_BalanceStock")]
+    partial class Add_BalanceStock
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
